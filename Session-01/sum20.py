ad36eca2a57beee4e9b64a34f81bf64b2bd05eab
mtorrego <m.torrego.2019@alumnos.urjc.es>
2020-02-04 15:37:55 +0100

    just another test

---
+++ Session-01/sum20.py
@@ -0,0 +1,4 @@
+a = 0
+for i in range(1, 21):
+    a = a + i
+print("Total sum is: ", a)
--- hello/hello.py
+++ hello/hello.py
@@ -1,2 +1,4 @@
+print("This is my first python program in pycharm")
+
 print("uuff")
 print("just testing...")
