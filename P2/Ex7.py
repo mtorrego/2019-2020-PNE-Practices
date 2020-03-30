96bab143b28b44c4b81166f975442b326f136024
mario <m.torrego.2019@alumnos.urjc.es>
2020-03-30 17:24:00 +0200

    Practise 2 finished

--- P2/Ex7.py
+++ P2/Ex7.py
@@ -2,17 +2,18 @@ from Client0 import Client
 from Seq1 import *
 
 PRACTICE = 2
-EXERCISE = 6
+EXERCISE = 7
 
 print(f"-----| Practice {PRACTICE}, Exercise {EXERCISE} |------")
 
-# -- Parameters of the server to talk to
 IP = "192.168.8.108"
-PORT = 8081
-
+PORT1 = 8080
+PORT2 = 8081
 # -- Create a client object
-c = Client(IP, PORT)
+c = Client(IP, PORT1)
+c1 = Client(IP, PORT2)
 print(c)
+print(c1)
 
 # -- Create a Null sequence
 s = Seq()
@@ -29,16 +30,26 @@ def function(times):
     counter = 0
     word = ""
     number = int((times/10) + 1)
-    while counter <= 5:
+    while counter <= 10:
         for base in str(a[times:times + 10]):
             word += base
             counter += 1
     return "Fragment " + str(number) + ": " + word
 
 
-c.debug_talk("Sending FRAT1 gene to the server in fragments of 10 bases")
-#c.debug_talk(str(a))
-
-listtimes = [0, 10, 20, 30, 40]
+listtimes = [0, 10, 20, 30, 40, 50, 60, 70, 80, 90]
+print("Gene FRAT1: ", a)
 for time in listtimes:
-    c.debug_talk(function(time))
+    print(function(time))
+
+
+listtimes1 = [10, 30, 50, 70, 90]
+for time in listtimes1:
+    c.talk(function(time))
+listtimes2 = [0, 20, 40, 60, 80]
+for time in listtimes2:
+    c1.talk(function(time))
+
+
+
+
